fc20345957dffd1d8bde038ef92c0ec9ad0e5fb6
Szymon Miks <miks.szymon@gmail.com>
2020-02-17 17:49:39 +0100

    [trello-50]: Changed font to Inter

--- src/App.vue
+++ src/App.vue
@@ -34,12 +34,34 @@ export default {
 </script>
 
 <style>
+@import url('https://rsms.me/inter/inter.css');
+
+html {
+  font-family: 'Inter', sans-serif;
+}
+
+@supports (font-variation-settings: normal) {
+  html {
+    font-family: 'Inter var', sans-serif;
+  }
+}
+
+button,
+input,
+select,
+textarea {
+  font-family: inherit;
+  font-size: inherit;
+  line-height: inherit;
+  color: inherit;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000;
 }
 
 body {
