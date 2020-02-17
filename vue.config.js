a8ef106796f4ed97fcd73b04035b81d0cc61038f
Szymon Miks <miks.szymon@gmail.com>
2020-02-17 18:42:57 +0100

    [master]: Disable PWA form vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -17,23 +17,23 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   chainWebpack: chainWebpackConfig,
   css: cssConfig,
-  pwa: {
-    workboxPluginMode: 'GenerateSW',
-    workboxOptions: {
-      navigateFallback: '/index.html',
-      // runtimeCaching: [
-      //   {
-      //     urlPattern: new RegExp('^https://api.neoguide.pl/api/v1/psalms/'),
-      //     handler: 'networkFirst',
-      //     options: {
-      //       networkTimeoutSeconds: 20,
-      //       cacheName: 'api-cache',
-      //       cacheableResponse: {
-      //         statuses: [0, 200]
-      //       }
-      //     }
-      //   }
-      // ]
-    }
-  }
+  // pwa: {
+  //   workboxPluginMode: 'GenerateSW',
+  //   workboxOptions: {
+  //     navigateFallback: '/index.html',
+  //     runtimeCaching: [
+  //       {
+  //         urlPattern: new RegExp('^https://api.neoguide.pl/api/v1/psalms/'),
+  //         handler: 'networkFirst',
+  //         options: {
+  //           networkTimeoutSeconds: 20,
+  //           cacheName: 'api-cache',
+  //           cacheableResponse: {
+  //             statuses: [0, 200]
+  //           }
+  //         }
+  //       }
+  //     ]
+  //   }
+  // }
 }
