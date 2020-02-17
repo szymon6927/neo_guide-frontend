a26e703c6b9c47378cb34373f4e50ad08fd2384e
Szymon Miks <miks.szymon@gmail.com>
2020-02-17 18:33:08 +0100

    [master]: Removed runtimeCaching form vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -21,19 +21,19 @@ module.exports = {
     workboxPluginMode: 'GenerateSW',
     workboxOptions: {
       navigateFallback: '/index.html',
-      runtimeCaching: [
-        {
-          urlPattern: new RegExp('^https://api.neoguide.pl/api/v1/psalms/'),
-          handler: 'networkFirst',
-          options: {
-            networkTimeoutSeconds: 20,
-            cacheName: 'api-cache',
-            cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        }
-      ]
+      // runtimeCaching: [
+      //   {
+      //     urlPattern: new RegExp('^https://api.neoguide.pl/api/v1/psalms/'),
+      //     handler: 'networkFirst',
+      //     options: {
+      //       networkTimeoutSeconds: 20,
+      //       cacheName: 'api-cache',
+      //       cacheableResponse: {
+      //         statuses: [0, 200]
+      //       }
+      //     }
+      //   }
+      // ]
     }
   }
 }
